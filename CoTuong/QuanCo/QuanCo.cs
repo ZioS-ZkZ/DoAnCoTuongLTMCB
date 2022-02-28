09b0798b0bee3e6559a64140e58e784418361813
ductt02 <bvsecret1311@gmail.com>
2022-02-28 13:58:28 +0700

    Tối ưu hoá thuật toán hiển thị mộ cờ

--- CoTuong/QuanCo/QuanCo.cs
+++ CoTuong/QuanCo/QuanCo.cs
@@ -10,12 +10,6 @@ namespace CoTuong.QuanCo
     {
         public int Hang;
         public int Cot;
-		public int toaDoMoDo_x;//toa do hien thi quan co bi an
-		public int toaDoMoDo_y;//toa do hien thi quan co bi an
-		public int toaDoMoDen_x;//toa do hien thi quan co bi an
-		public int toaDoMoDen_y;//toa do hien thi quan co bi an
-		public static List<QuanCo> dsCoChet_Do = new List<QuanCo>();
-		public static List<QuanCo> dsCoChet_Den = new List<QuanCo>();
 		public string Ten;
         public int Phe;
         public string Phia;// phia ben trai hoac phai VD: xe-trai, xe-phai
